e7ca74da6d59a10824f98d5a5965a9f7bb5aaf2a
pagepan <pagepan@qq.com>
2024-08-17 20:24:56 +0800

    update

---
+++ src/app/api/v2/item/export.php
@@ -0,0 +1,27 @@
+<?php
+return function ($request_data) {
+    $table = $request_data['table'] ?: 'item';
+    $column = $request_data['column'] ?: '*';
+    $where = $request_data['where'] ?: '1 = 1';
+    $order = $request_data['order'] ?: ['sortby' => 'DESC', 'ctime' => 'DESC'];
+
+    $db = db();
+    $db->debug = false;
+    $items = $db->select($table, $column, $where, $order);
+    foreach ($items as $i => $item) {
+        $pid = $item['pid'];
+        if (!empty($pid)) {
+            $items[$i]['category'] = $db->find($table, 'title', ['id', '=', $pid], null, 0);
+        }
+        $ctime = $item['ctime'];
+        if (!empty($ctime)) {
+            $items[$i]['ctime'] = date('Y-m-d H:i:s', $ctime);
+        }
+        $utime = $item['utime'];
+        if (!empty($utime)) {
+            $items[$i]['utime'] = date('Y-m-d H:i:s', $utime);
+        }
+    }
+
+    Response::success('', $items);
+};
