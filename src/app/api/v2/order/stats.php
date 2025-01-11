07fae78491a1e936766a0f0bc293abd5a5a2c1d1
pagepan <pagepan@qq.com>
2025-01-11 17:41:18 +0800

    update

--- src/app/api/v2/order/stats.php
+++ src/app/api/v2/order/stats.php
@@ -2,13 +2,12 @@
 return function ($request_data) {
     $db = db();
     $db->debug = false;
-    $today_count = $db->query("select count(id) from `order` where strftime('%Y-%m-%d', datetime(ctime, 'unixepoch')) = strftime('%Y-%m-%d', 'now');", [], 0);
-    $today_total = $db->query("select sum(total) from `order` where strftime('%Y-%m-%d', datetime(ctime, 'unixepoch')) = strftime('%Y-%m-%d', 'now');", [], 0);
-    $yesterday_total = $db->query("select sum(total) from `order` where strftime('%Y-%m-%d', datetime(ctime, 'unixepoch')) = strftime('%Y-%m-%d', 'now', '-1 day');", [], 0);
+    $day = intval($request_data['day']);
+    $today_count = $db->query("select count(id) from `order` where strftime('%Y-%m-%d', datetime(ctime, 'unixepoch')) = strftime('%Y-%m-%d', 'now', '{$day} day');", [], 0);
+    $today_total = $db->query("select sum(total) from `order` where strftime('%Y-%m-%d', datetime(ctime, 'unixepoch')) = strftime('%Y-%m-%d', 'now', '{$day} day');", [], 0);
 
     Response::success('订单统计', [], [
         'today_count' => $today_count ?? 0,
         'today_total' => $today_total ?? 0,
-        'yesterday_total' => $yesterday_total ?? 0
     ]);
 };
