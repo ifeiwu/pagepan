275e21f545941b7f565a6e90dc6834c334dd7f60
pagepan <pagepan@qq.com>
2025-03-17 21:13:11 +0800

    update

--- src/public/assets/js/app/plugins/plyr.js
+++ src/public/assets/js/app/plugins/plyr.js
@@ -1,8 +1,6 @@
 /**
- * 视音频播放组件
- * <video class="plyr" width="100%" poster="<?=$this->uikit->item_image($path, $image); ?>" preload="none" controls="" playsInline="">
- *     <source src="<?=$this->uikit->item_image($path, $item['video']); ?>"></source>
- * </video>
+ * Plyr 视音频播放组件
+ * <video src="<?=$this->uikit->item_image($path, $item['video']);?>" class="plyr" width="100%" poster="<?=$this->uikit->item_image($path, $image);?>" preload="none" controls playsInline></video>
  **/
 define(function (require) {
 
@@ -36,22 +34,30 @@ define(function (require) {
     });
 
     // 支持流媒体 Hls
-    if ($('audio.plyr[hls-src$=".m3u8"], video.plyr[hls-src$=".m3u8"]').length) {
-        require(['hls'], function (Hls) {
-            $('audio.plyr[hls-src$=".m3u8"], video.plyr[hls-src$=".m3u8"]').each(function (i, el) {
-                let url = $(el).attr('hls-src');
+    if ($('audio.plyr[src$=".m3u8"], video.plyr[src$=".m3u8"]').length) {
+        require(['hls/hls'], function (Hls) {
+            $('audio.plyr[hls-src$=".m3u8"], video.plyr[src$=".m3u8"]').each(function (i, el) {
+                let url = $(el).attr('src');
                 if (Hls.isSupported()) {
                     let hls = new Hls();
                     hls.loadSource(url);
                     hls.attachMedia(el);
-                    hls.on(Hls.Events.MANIFEST_PARSED, function () {
-                        el.play();
-                    });
+                    if (el.autoplay == true) {
+                        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+                            el.play();
+                        });
+                    } else {
+                        el.pause();
+                    }
                 } else if (el.canPlayType('application/vnd.apple.mpegurl')) {
                     el.src = url;
-                    el.addEventListener('loadedmetadata', function () {
-                        el.play();
-                    });
+                    if (el.autoplay == true) {
+                        el.addEventListener('loadedmetadata', function () {
+                            el.play();
+                        });
+                    } else {
+                        el.pause();
+                    }
                 }
             });
         });
