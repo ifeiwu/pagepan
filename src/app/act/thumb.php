228a4d13f583e0062204f7fb7f8ad34de51d0660
pagepan <pagepan@qq.com>
2025-03-26 21:16:39 +0800

    update

--- src/app/act/thumb.php
+++ src/app/act/thumb.php
@@ -1,21 +1,34 @@
 <?php
 /**
  * 生成缩略图片
- * <img src="img/data/file/bg/ad.png?w=500&h=200&fit=crop-center">
+ * <img src="img/data/file/ad.png?p=600">
  */
-
 require_once VEN_PATH . 'autoload.php';
-
-use League\Glide\Signatures\SignatureFactory;
-use League\Glide\Signatures\SignatureException;
 
 return function ($name) {
     try {
         $server = League\Glide\ServerFactory::create([
-            'source' => WEB_ROOT,
-            'cache' => CACHE_PATH,
+            'source' => WEB_ROOT, // 原始图片存储路径
+            'cache' => CACHE_PATH, // 缓存路径
+            'max_image_size' => 4000 * 4000,// 通过设置 max_image_size 参数，可以限制生成的图片的最大尺寸，避免生成过大的图片。
+            // 预设可以提前定义常用的图片处理参数，避免在每次请求时重复设置参数。
+            'presets' => [
+                '200' => [
+                    'w' => 200,
+                    'fm' => 'webp',
+                ],
+                '600' => [
+                    'w' => 600,
+                    'fm' => 'webp',
+                ],
+                '1200' => [
+                    'w' => 1200,
+                    'fm' => 'webp',
+                ]
+            ]
         ]);
-        $server->outputImage($name, $_GET);
+        // 为了安全起见只能使用预设，避免生成过多尺寸的图片。
+        $server->outputImage($name, ['p' => $_GET['p']]);
     } catch (Exception $ex) {
         throw $ex;
     }
