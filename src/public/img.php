f2f61e8b4e5854bfd569f70753cddb03455002d8
pagepan <pagepan@qq.com>
2025-06-16 12:04:02 +0800

    update

--- src/public/img.php
+++ src/public/img.php
@@ -5,15 +5,12 @@ define('ROOT_PATH', dirname(WEB_ROOT) . '/');
 require ROOT_PATH . 'base.php';
 require VEN_PATH . 'autoload.php';
 
-\Co\async(function () {
-    $server = Helper::glide_server();
-    // 清理指定图片缓存
-    if (isset($_GET['dc'])) {
-        $server->deleteCache($_GET['path']);
-    }
-    // 为了安全起见只能使用预设，避免生成过多尺寸的图片。
+$server = Helper::glide_server();
+// 清理指定图片缓存
+if (isset($_GET['dc'])) {
+    $server->deleteCache($_GET['path']);
+}
+// 为了安全起见只能使用预设，避免生成过多尺寸的图片。
 //    $path = preg_replace('/[<>:"|?*]+/', '', $_GET['path']);
-    $preset = intval($_GET['p']);
-    $server->outputImage($_GET['path'], ['p' => $preset]);
-});
-\Co\wait();
+$preset = intval($_GET['p']);
+$server->outputImage($_GET['path'], ['p' => $preset]);
