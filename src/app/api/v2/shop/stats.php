3300a23f2f333533ec6cb902c76a36086bfdf726
pagepan <pagepan@qq.com>
2025-06-23 17:51:14 +0800

    update

--- src/app/api/v2/shop/stats.php
+++ src/app/api/v2/shop/stats.php
@@ -1,32 +1,30 @@
 <?php
 return function ($request_data) {
-    $date = $request_data['date'];
+    $tab = $request_data['tab'];
     $where = "page_url LIKE '/shop%'";
-    switch ($date) {
-        case '24hour': // 获取 24 小时前的时间戳
-            $hour = time() - (24 * 60 * 60);
-            $where = "{$where} AND visit_time >= {$hour}";
-            break;
+    switch ($tab) {
         case 'yesterday':
-            $yesterday = date('Y-m-d', strtotime('-1 day')); // 获取昨天的日期
-            $where = "{$where} AND strftime('%Y-%m-%d', visit_time, 'unixepoch', 'localtime') = '{$yesterday}'";
-            $sql_chart = "SELECT strftime('%Y-%m-%d %H:00', visit_time, 'unixepoch', 'localtime') AS hour, COUNT(*) AS views, COUNT(DISTINCT visitor_id) AS visitors FROM event WHERE {$where} GROUP BY hour ORDER BY hour DESC";
+            $where = "{$where} AND DATE(visit_time, 'unixepoch') = DATE('now', '-1 day')";
+            $sql_chart = "SELECT strftime('%Y-%m-%d %H:00', visit_time, 'unixepoch', 'localtime') AS vtime, COUNT(*) AS views, COUNT(DISTINCT visitor_id) AS visitors FROM event WHERE {$where} GROUP BY vtime ORDER BY vtime DESC";
             break;
         case 'week':
-            // 确定本周的开始日期
-            $start_of_week = strtotime('monday this week');
-            $start_date = date('Y-m-d', $start_of_week);
-            $where = "{$where} AND DATE(timestamp, 'unixepoch', 'localtime') >= '$start_date'";
-            $sql_chart = "SELECT strftime('%Y-%m-%d', visit_time, 'unixepoch', 'localtime') AS hour, COUNT(*) AS views, COUNT(DISTINCT visitor_id) AS visitors FROM event WHERE {$where} GROUP BY hour ORDER BY hour DESC";
+            $where = "{$where} AND visit_time BETWEEN strftime('%s', 'now', 'weekday 0', '-7 days') AND strftime('%s', 'now', 'weekday 0', '-1 days')";
+            $sql_chart = "SELECT strftime('%Y-%m-%d', visit_time, 'unixepoch', 'localtime') AS vtime, COUNT(*) AS views, COUNT(DISTINCT visitor_id) AS visitors FROM event WHERE {$where} GROUP BY vtime ORDER BY vtime DESC";
+            break;
+        case 'month':
+            $where = "{$where} AND strftime('%Y-%m', datetime(visit_time, 'unixepoch')) = strftime('%Y-%m', 'now', 'localtime')";
+            $sql_chart = "SELECT strftime('%Y-%m-%d', visit_time, 'unixepoch', 'localtime') AS vtime, COUNT(*) AS views, COUNT(DISTINCT visitor_id) AS visitors FROM event WHERE {$where} GROUP BY vtime ORDER BY vtime DESC";
+            break;
+        case 'year':
+            $where = "{$where} AND strftime('%Y', datetime(timestamp, 'unixepoch')) = strftime('%Y', 'now')";
+            $sql_chart = "SELECT strftime('%Y-%m-%d', visit_time, 'unixepoch', 'localtime') AS vtime, COUNT(*) AS views, COUNT(DISTINCT visitor_id) AS visitors FROM event WHERE {$where} GROUP BY vtime ORDER BY vtime DESC";
             break;
         default:
-            $today_start = strtotime('today');
-            $today_end = strtotime('tomorrow');
-            $where = "{$where} AND visit_time >= $today_start AND visit_time < $today_end";
-            $sql_chart = "SELECT strftime('%Y-%m-%d %H:00', visit_time, 'unixepoch', 'localtime') AS hour, COUNT(*) AS views, COUNT(DISTINCT visitor_id) AS visitors FROM event WHERE {$where} GROUP BY hour ORDER BY hour DESC";
+            $where = "{$where} AND DATE(visit_time, 'unixepoch', 'localtime') = DATE('now', 'localtime')";
+            $sql_chart = "SELECT strftime('%Y-%m-%d %H:00', visit_time, 'unixepoch', 'localtime') AS vtime, COUNT(*) AS views, COUNT(DISTINCT visitor_id) AS visitors FROM event WHERE {$where} GROUP BY vtime ORDER BY vtime DESC";
             break;
     }
-    debug($sql_chart);
+
     $sql_views = "SELECT COUNT(*) FROM event WHERE {$where}";
     $sql_visits = "SELECT COUNT(DISTINCT visit_id) FROM event WHERE {$where}";
     $sql_visitors = "SELECT COUNT(DISTINCT visitor_id) FROM event WHERE {$where}";
@@ -38,7 +36,7 @@ return function ($request_data) {
     $visits = $db->querySingle($sql_visits); // 访问次数
     $visitors = $db->querySingle($sql_visitors); // 独立访客
     $items = $db->query($sql_items); // 商品浏览排行
-    $chart_data = _getCharts($db, $sql_chart, $date); // 图表数据
+    $chart_data = _getCharts($db, $sql_chart, $tab); // 图表数据
 
     $db2 = new SQLite3(ROOT_PATH . 'data/sqlite/pagepan.db');
     while ($row = $items->fetchArray(SQLITE3_ASSOC)) {
@@ -66,42 +64,46 @@ return function ($request_data) {
     Response::json(array_merge($data, ['chart_data' => $chart_data]));
 };
 
-function _getCharts($db, $sql_chart, $date)
+function _getCharts($db, $sql_chart, $tab)
 {
+    $unit = 'hour';
     $lables = [];
     $visitors = [];
     $views = [];
 
-    // 查找12个小时段的访问数据
+    // 查找到的访问时间
     $result = $db->query($sql_chart);
-    // 将查询结果存储到数组中
-    $hourly = [];
+    $vtimes = [];
     while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-        $hourly[$row['hour']] = ['visitors' => $row['visitors'], 'views' => $row['views']];
+        $vtimes[$row['vtime']] = ['visitors' => $row['visitors'], 'views' => $row['views']];
     }
 
-    if ($date == 'today') {
-        $hours = _getTodayHours();
-    } elseif ($date == 'yesterday') {
-        $hours = _getYesterdayHours();
+    // 获取所有时间段
+    $alltime = [];
+    if ($tab == 'today') {
+        $alltime = _getTodayHours();
+    } elseif ($tab == 'yesterday') {
+        $alltime = _getYesterdayHours();
+    } elseif ($tab == 'week') {
+        $alltime = _getPastDaysOfWeek();
+    } elseif ($tab == 'month') {
+        $alltime = _getDaysInMonth();
+    } elseif ($tab == 'year') {
+        $alltime = _getYearMonths();
     }
-
-
-
-    // 填充缺失的小时数
-    $hours = array_reverse($hours);
-    foreach ($hours as $hour) {
-        $lables[] = $hour;
-        if (isset($hourly[$hour])) {
-            $visitors[] = $hourly[$hour]['visitors'];
-            $views[] = $hourly[$hour]['views'];
+    // 填充没有的访问时间
+    foreach ($alltime as $time) {
+        $lables[] = $time;
+        if (isset($vtimes[$time])) {
+            $visitors[] = $vtimes[$time]['visitors'];
+            $views[] = $vtimes[$time]['views'];
         } else {
             $visitors[] = 0;
             $views[] = 0;
         }
     }
 
-    return ['lables' => $lables, 'visitors' => $visitors, 'views' => $views];
+    return ['lables' => $lables, 'visitors' => $visitors, 'views' => $views, 'tab' => $tab];
 }
 
 // 获取今天的时间段
@@ -115,7 +117,7 @@ function _getTodayHours()
         $hour = date('Y-m-d H:00', strtotime(date('Y-m-d ' . $i . ':00')));
         $hours[] = $hour;
     }
-    return $hours;
+    return array_reverse($hours);
 }
 
 // 获取昨天的时间段
@@ -129,23 +131,46 @@ function _getYesterdayHours()
         $hours[] = date('Y-m-d H:00', $timestamp);
     }
 
-    return $hours;
+    return array_reverse($hours);
 }
 
 // 获取本周已过天数的数组
-function getPastDaysOfWeek()
+function _getPastDaysOfWeek()
 {
-    $today = new DateTime(); // 获取当前日期时间
-    $currentDayOfWeek = $today->format('w'); // 获取今天是星期几，0 表示星期日，1-6 表示星期一到星期六
+    $dateTime = new DateTime('monday this week');
+    $days = [];
 
-    $pastDays = [];
-    for ($i = 0; $i < $currentDayOfWeek; $i++) {
-        $date = clone $today; // 克隆当前日期，避免修改原始对象
-        $date->modify("-$i days"); // 往前推 $i 天
-        $pastDays[] = $date->format('Y-m-d'); // 格式化日期并添加到数组
+    for ($i = 0; $i < 7; $i++) {
+        $days[] = $dateTime->format('Y-m-d');
+        $dateTime->modify('+1 day');
     }
 
-    return array_reverse($pastDays); // 反转数组，使日期按时间先后顺序排列
+    return $days;
+}
+
+// 获取本月已过天数
+function _getDaysInMonth()
+{
+    $today = new DateTime(); // 获取当前日期
+    $year = (int)$today->format('Y'); // 获取年份
+    $month = (int)$today->format('m'); // 获取月份
+    $day = (int)$today->format('d');   // 获取当前是哪一天
+
+    $days = [];
+    for ($i = 1; $i <= $day; $i++) {
+        $days[] = sprintf('%04d-%02d-%02d', $year, $month, $i); // 格式化日期
+    }
+
+    return array_reverse($days);
+}
+
+function _getYearMonths()
+{
+    $months = [];
+    for ($i = 1; $i <= 12; $i++) {
+        $months[] = date('F', mktime(0, 0, 0, $i, 1, date('Y')));
+    }
+    return $months;
 }
 
 // 格式化数字
