1e2995b84ea211e517e25469958f48ebe1ed6efb
pagepan <pagepan@qq.com>
2025-08-04 09:29:31 +0800

    update

--- src/public/assets/js/app/plugins/h-grow.js
+++ src/public/assets/js/app/plugins/h-grow.js
@@ -4,5 +4,9 @@ define(function (require) {
         let offset_top = el.offsetTop;
         offset_top = offset_top < document.body.offsetHeight ? offset_top : 0;
         el.style.setProperty('--offset-top', offset_top + 'px');
+
+        let offset_bottom = document.body.offsetHeight - (offset_top + el.offsetHeight);
+        offset_bottom = offset_bottom > 0 ? offset_bottom : 0;
+        el.style.setProperty('--offset-bottom', offset_bottom + 'px');
     });
 });
