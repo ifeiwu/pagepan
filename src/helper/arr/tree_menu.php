1056e6da0e1c12e1f2225904e9de340b3d7f898c
pagepan <pagepan@qq.com>
2025-12-06 17:40:09 +0800

    update

--- src/helper/arr/tree_menu.php
+++ src/helper/arr/tree_menu.php
@@ -2,8 +2,7 @@
 // 构建树形菜单
 return $tree_menu_fun = function ($trees, &$element, $cur_level = 0, $max_level, $value, $options) use (&$tree_menu_fun) {
 
-    if ( $cur_level < $max_level && is_array($trees) )
-    {
+    if ($cur_level < $max_level && is_array($trees)) {
         foreach ($trees as $tree) {
             $child_name = $options['child_name'];
             $child_name = $child_name ?: '_child';
@@ -15,19 +14,19 @@ return $tree_menu_fun = function ($trees, &$element, $cur_level = 0, $max_level,
             $target = $options['target'] ?: $tree['link_target'];
             $active = $value == $id ? $options['active'] : '';
 
-            if ( ! empty($options['url']) ) {
+            if (!empty($options['url'])) {
                 $url = str_replace('{id}', $id, $options['url']);
             } else {
                 $url = $tree['link_url'];
             }
 
-            if ( $_child ) {
+            if ($_child) {
                 $element .= '<li class="' . $active . '" data-pid="' . $pid . '" data-id="' . $id . '">';
-                $element .= '<a href="' . ($options['arrow_add_url'] ? $url : '#') . '" class="' . $options['arrow'] . '" aria-expanded="' . ($active ? 'true' : 'false') . '">' . $title . '</a>';
+                $element .= '<a href="' . ($url ? $url : '#') . '" class="' . $options['arrow'] . '" aria-expanded="' . ($active ? 'true' : 'false') . '">' . $title . '</a>';
                 $element .= '<ul class="list-none">';
 
                 $tree_menu_fun($_child, $element, $cur_level + 1, $max_level, $value, $options);
-                
+
                 $element .= '</ul>';
                 $element .= '</li>';
             } else {
--- src/public/assets/js/util/class-active.js
+++ src/public/assets/js/util/class-active.js
@@ -2,7 +2,12 @@ define(function () {
     return function (selector) {
         const $component = $(selector);
         if (!pagevar.isBuilder) {
-            let $li = $component.find('a[href="' + pagevar.page_alias + '"],a[href*="/category/' + pagevar.get_pid + '"]').parent('li').addClass('active');
+            let $li = $component.find('a[href="' + pagevar.page_alias + '"]').parent('li').addClass('active');
+
+            if (pagevar.get_cid) {
+                $li = $component.find('a[href*="' + pagevar.page_alias + '/category/' + pagevar.get_cid + '"]').parent('li').addClass('active');
+            }
+
             if (!$li.is('.active')) {
                 let alias = pagevar.join_alias ? pagevar.join_alias : pagevar.page_alias ? pagevar.page_alias : '';
                 alias = alias.indexOf('/') > 0 ? alias.split('/')[0] : alias;
@@ -10,10 +15,12 @@ define(function () {
             }
 
             if (!$li.is('.active')) {
-                $component.find('a[active~="' + pagevar.page_alias + '"],a[active~="' + pagevar.join_alias + '"],a[active="' + pagevar.get_pid + '"]').parent('li').addClass('active');
+                $component.find('a[active~="' + pagevar.page_alias + '"],a[active~="' + pagevar.join_alias + '"],a[active="' + pagevar.get_cid + '"]').parent('li').addClass('active');
             }
 
-            $component.find('a[href="#"],a[href="/"],a[href="./"],a[href=""]').parent('li').addClass('active');
+            if (!pagevar.page_alias) {
+                $component.find('a[href="/"],a[href="./"]').parent('li').addClass('active');
+            }
         }
     };
 });
