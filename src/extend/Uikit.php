862caec0670957a386134b83cb1447cedbee62d0
pagepan <pagepan@qq.com>
2026-05-17 15:13:10 +0800

    update

--- src/extend/Uikit.php
+++ src/extend/Uikit.php
@@ -510,7 +510,7 @@ class Uikit
     }
 
     // item 返回转换的内容
-    public function item_content($content, $content_type = 'md')
+    public function item_content($content, $content_type = 'md', $utime = null)
     {
         if ($content_type == 'md') {
             $content = (new Parsedown())->text(html_decode($content));
@@ -518,8 +518,11 @@ class Uikit
         } else {
             $content = html_decode($content);
         }
+
+        $utime = $utime ? "?$utime" : '';
         $pattern = '/<img(.*?)[^>]src=["\'](.+?\.(jpg|jpeg|png|webp|avif|bmp|gif|svg))["\'][^>](.*?)>/i';
-        $replace = "<img class=\"lazyload zooming\" data-src=\"$2\" src=\"assets/image/loading.svg\" $1 $4>";
+        $replace = "<img class=\"zooming\" loading=\"lazy\" src=\"$2{$utime}\" $1 $4>";
+
         return preg_replace($pattern, $replace, $content);
 //        return preg_replace('/<img.+?src=[\'"](.+?\.(jpg|jpeg|png|webp|avif|bmp|gif|svg))[\'"](.*?)>/i', "<img class=\"lazyload zooming\" data-src=\"$1\" src=\"assets/image/loading.svg\" $3>", $content);
     }
